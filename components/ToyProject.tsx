e1bf9d1bbbcef4c44cde24bbe48634a8d3378741
YejeeCho <choyejee14@gmail.com>
2022-10-05 13:38:32 +0900

    toyproject 내용 추가

--- components/ToyProject.tsx
+++ components/ToyProject.tsx
@@ -31,7 +31,9 @@ export const ToyProject = () => {
             member="프론트엔드 1명"
             skills="HTML/CSS, JavaScript"
             myJob="전체 페이지 기획 및 개발"
-            retrospection="HTML과 CSS 기초만 익히고 처음으로 제작해본 웹사이트였기 떄문에 부족한 점도 많았지만, 개발자로서의 첫걸음이라는 점에서 의미가 크다고 생각합니다."
+            text1="- HTML/CSS로 정적인 페이지 구현"
+            text2="- 자바스크립트의 offset을 이용해 nav바 각 버튼 클릭시 해당 페이지로 스크롤되는 기능 구현"
+            retrospection="HTML과 CSS의 사용법에 대해 익힐 수 있었고, 시멘틱 태그의 의미에 대해 알게 되었습니다. style 값을 조정하는 데 어려움이 많았는데, css의 다양한 속성과 선택자의 우선순위에 대해서 한번 더 공부할 수 있었습니다."
             github="https://github.com/Dumibell/self_introduction"
             blog="https://velog.io/@dumibell/%EC%9E%90%EA%B8%B0%EC%86%8C%EA%B0%9C-%ED%8E%98%EC%9D%B4%EC%A7%80-%EB%A7%8C%EB%93%A4%EA%B8%B0HTML-CSS"
           />
