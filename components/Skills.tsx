39963a777a8a457e1d88d45644c7d656a82345c6
YejeeCho <choyejee14@gmail.com>
2023-08-25 17:42:45 +0900

    주석처리한 코드 제거 및 문구 수정

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,108 +1,6 @@
 import { styled } from "styled-components";
 import { mobile } from "../styles/theme";
 
-// export const Skills = () => {
-//   return (
-//     <div className="h-screen w-screen flex flex-col justify-center" id="2">
-//       <div className="m-20 flex flex-col items-center font-Roboto text-3xl">
-//         <div className="mt-5 flex flex-col items-center">
-//           <p className="underline decoration-2 mb-2">
-//             Frontend / Backend Skills
-//           </p>
-//           <div>
-//             <div className="flex flex-col items-center my-2">
-//               <div className="flex">
-//                 <Icons src="/images/html5.svg" skill="HTML" />
-//                 <Icons src="/images/css3.svg" skill="CSS" />
-//               </div>
-//               <p className="text-sm">
-//                 웹디자이너의 요구사항을 UI에 반영할 수 있습니다.
-//               </p>
-//             </div>
-//             <div className="flex flex-col items-center my-2">
-//               <div className="flex">
-//                 <Icons src="/images/javascript.svg" skill="JavaScript" />
-//                 <Icons src="/images/react.svg" skill="React.js" />
-//               </div>
-//               <p className="text-sm">
-//                 자바스크립트와 React.js를 통해 페이지를 제작한 경험이 있습니다.
-//               </p>
-//             </div>
-//             <div className="flex flex-col items-center my-2">
-//               <div className="flex">
-//                 <Icons
-//                   src="/images/styledcomponents.svg"
-//                   skill="Styled-Components"
-//                 />
-//               </div>
-//               <p className="text-sm">
-//                 props를 활용해 속성값을 변경할 수 있습니다.
-//               </p>
-//             </div>
-//             <div className="flex flex-col items-center my-2">
-//               <div className="flex">
-//                 <Icons src="/images/sass.svg" skill="Sass" />
-//               </div>
-//               <p className="text-sm">
-//                 nesting 기능을 활용하여 코드를 효율적으로 작성할 수 있습니다.
-//               </p>
-//             </div>
-//             <div className="flex flex-col items-center my-2">
-//               <div className="flex">
-//                 <Icons src="/images/tailwindcss.svg" skill="TailwindCss" />
-//               </div>
-//               <p className="text-sm">
-//                 해당 포트폴리오를 tailwindcss를 사용해 제작하였습니다.
-//               </p>
-//             </div>
-//             <div className="flex flex-col items-center my-2">
-//               <div className="flex">
-//                 <Icons src="/images/firebase.svg" skill="Firebase" />
-//               </div>
-//               <p className="text-sm">
-//                 Firebase의 Authentication, Storage, Firestore 기능을 활용해 DB를
-//                 관리할 수 있습니다.
-//               </p>
-//             </div>
-//           </div>
-//           <div className=" flex flex-col items-center mt-8">
-//             <p className="underline decoration-2 mb-2">Business Tools</p>
-//             <div className="mt-1 flex">
-//               <Icons src="/images/github.svg" skill="Github" />
-//               <Icons src="/images/notion.svg" skill="Notion" />
-//               <Icons src="/images/slack.svg" skill="Slack" />
-//               <Icons src="/images/trello.svg" skill="Trello" />
-//               {/* <p className="text-lg">Github, Notion, Slack, Trello</p> */}
-//             </div>
-//             <div className="text-sm">
-//               팀원 간의 협업 툴로 slack과 notion을 사용했으며 Trello를 통해
-//               진행상황을 공유했습니다.
-//             </div>
-//             {/* <p className="text-xl">Github, Notion, Slack, Trello</p> */}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// interface IconsType {
-//   src: string;
-//   skill: string;
-//   text?: string;
-// }
-
-// const Icons = ({ src, skill, text }: IconsType) => {
-//   return (
-//     <div className="text-xl flex flex-col items-center justify-center my-1">
-//       <div className="mx-1 flex">
-//         <Image src={src} alt="그림" width={15} height={15} />
-//         <p>{skill}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
 export const Skills = () => {
   return (
     <Container id="2">
@@ -161,7 +59,7 @@ export const Skills = () => {
           <Description>
             <p>
               - <span className="skill">Git</span>: gitflow에 대해 이해하며,
-              github & gitlab을 사용해 협업을 진행한 경험이 있습니다.
+              github & gitlab을 사용해 협업을 진행합니다.
             </p>
             <p>
               - <span className="skill">Figma & AdobeXd</span>: 디지이너와의
